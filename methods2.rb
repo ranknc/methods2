b6b2cd2b41c396704f2b2cc39d2f6a2b273c9a42
Nick Rank <ranknc@s.dcsdk12.org>
2015-09-01 10:12:01 -0600

    a

--- methods2.rb
+++ methods2.rb
@@ -1,11 +1,36 @@
-module Methods2
+ module Methods2
 	
 	# TODO - write elevenish?
-	
+
+	def elevenish(e)
+		if (e == 11) && (e == 22) && (e == 33) + 1
+			true
+		else (e <= 10) && (13 <= e <= 21) && (24 <= e <= 32)
+			false
+		end
+		
+	end
 	# TODO - write ice_cream_party?
+	def ice_cream_party(a, b, c)
+		if (a >= 4)
+			false
+		else (a <= 5)
+			true
+		end
+	
+	end
 	
 	# TODO - write successful_squirrel_party?
-	
+	def successful_squirrel_party(nuts, weekend)
+		if !weekend
+			nuts >= 40 && nuts <= 60
+			
+		else
+			nuts <= 39
+			
+		end
+			
+	end
 	# TODO - write ticket
 
 	# TODO - write in_order?
--- methods2_test.rb
+++ methods2_test.rb
@@ -2,13 +2,119 @@ require 'minitest/autorun'
 require "minitest/reporters"
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-require_relative 'methods'
+require_relative 'methods2'
 
 class Methods2Test < MiniTest::Test
 	def setup
 		@m = Class.new do
-     include Methods
+     include Methods2
    	end.new
 	end
 
+	def test_elevenish
+
+   	assert_equal false, @m.elevenish(1)
+   	assert_equal false, @m.elevenish(2)
+   	assert_equal true, @m.elevenish(11)
+   	assert_equal true, @m.elevenish(12)
+   	assert_equal true, @m.elevenish(22)
+   	assert_equal true, @m.elevenish(23)
+   	assert_equal true, @m.elevenish(33)
+   	assert_equal true, @m.elevenish(34)
+	
+	end
+	def test_ice_cream_party
+		assert_equal 0, @m.(0, 1, 2, 3, 4)
+   	assert_equal 1, @m.(5, 6, 7, 8, 9)
+   	assert_equal 2, @m.(10)
+   	
+		
+	end
+	def test_successful_squirrel_party
+		assert_equal true, @m.(70, true)
+   	assert_equal false, @m.(70, false)
+   	assert_equal true, @m.(50, true)
+   	assert_equal true, @m.(50, false)
+
+		
+	end
+	def test_ticket
+		assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+		
+	end
+	def test_in_order
+		assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+		
+	end
+	def test_less_by_ten
+		assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+		
+	end
+	def test_fizz_string
+		assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+		
+	end
+	def test_first_last_six
+		assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+		
+	end
+	def test_rotate_left
+		assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+		
+	end
+	def test_
+		assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+   	assert_equal , @m.()
+		
+	end
+
+	
 end
